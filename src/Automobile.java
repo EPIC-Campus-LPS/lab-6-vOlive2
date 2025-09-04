fc8477a0d24b4e0f0ce6a957117dd321723d7880
vOlive2.0 <OLiveA148@lpsk12.org>
2025-09-04 17:13:31 -0600

    Update Automobile.java

--- src/Automobile.java
+++ src/Automobile.java
@@ -1,5 +1,37 @@
 public class Automobile {
 	private String make;
-	private String modle;
+	private String model;
 	private double mileage;
+	private double fuel;
+	private double fuelTank;
+
+	Automobile(String mk, String mdl, double m, double fT) {
+		make = mk;
+		model = mdl;
+		mileage = m;
+		fuelTank = fT;
+	}
+	public void fillUp(double gallons) {
+		fuel += gallons;
+		if (fuel > fuelTank) {
+			fuel = fuelTank;
+		}
+	}
+	public void takeTrip(double miles) {
+		int c = 0;
+		boolean run = true;
+		while(run) {
+			c++;
+			if(c == miles) {
+				break;
+			} else {
+				if(fuel == 0) {
+					System.out.print("YOU RAN OUT OF FUEL! YOU NEEDED" + (miles - c) + " MORE GALLON(S) OF FUEL!");
+				}
+			}
+		}
+	}
+	public double reportFuel() {
+		return fuel;
+	}
 }
