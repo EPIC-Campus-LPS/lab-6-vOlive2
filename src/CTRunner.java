c2eeec100f8196719be23b62d1814eaf11422bdc
vOlive2.0 <OLiveA148@lpsk12.org>
2025-09-09 09:15:55 -0600

    Update CTRunner.java

--- src/CTRunner.java
+++ src/CTRunner.java
@@ -2,8 +2,12 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 public class CTRunner {
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws IOException {
+		File file = new File("part.txt");
+		Scanner scan = new Scanner(file);
+		while(scan.hasNextLine()) {
+			
+		}
 
 	}
 
